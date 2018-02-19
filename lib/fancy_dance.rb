01e66a14d1e233dec125d80c7d65662ec3962f29
Michael Kang <michaelkang85@gmail.com>
2018-02-19 22:37:09 +0000

    automatically backed up by learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,6 @@
+
+angelina = Kid.new("Angelina")
+mikhail_baryshnikov = Dancer.new("Mikhail")
+
+puts "#{angelina.name} says: #{angelina.twirl}"
+puts "#{mikhail_baryshnikov.name} says: #{mikhail_baryshnikov.take_a_bow}"
